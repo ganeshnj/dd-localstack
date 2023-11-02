e30d5cee0305716a4a9d554116df22c4c5fe93c2
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2023-11-02 14:25:17 +0100

    add go example app and update instructions

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 ## How to use
 
-```rust
+### Local server
+
+```
 cargo build
 cargo run
 ```
+
+### Examples
+
+#### Go example
+
+```
+cd examples/go-example
+go build
+go run .
+```
+
+Check server logs and `/traces` directory.
---
+++ examples/go-example/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	ddotel "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/opentelemetry"
+)
+
+func operationThatCouldFail() (string, error) {
+	return "", errors.New("empty name")
+}
+
+func main() {
+	provider := ddotel.NewTracerProvider()
+	defer provider.Shutdown()
+
+	otel.SetTracerProvider(provider)
+
+	tracer := otel.Tracer("")
+	ctx, span := tracer.Start(
+		context.Background(),
+		"span_name",
+		trace.WithAttributes(attribute.String("hello", "world")))
+	span.SetAttributes(attribute.Bool("isTrue", true), attribute.String("stringAttr", "hi!"))
+	var myKey = attribute.Key("myCoolAttribute")
+	span.SetAttributes(myKey.String("a value"))
+	span.AddEvent("Acquiring lock")
+	span.AddEvent("Got lock, doing work...")
+	span.AddEvent("Unlocking")
+	span.AddEvent("Cancelled wait due to external signal", trace.WithAttributes(attribute.Int("pid", 4328), attribute.String("signal", "SIGHUP")))
+	span.SetStatus(codes.Error, "operationThatCouldFail failed")
+	_, err := operationThatCouldFail()
+	span.RecordError(err)
+
+	_, childSpan := tracer.Start(ctx, "child_span")
+	childSpan.End()
+	span.End()
+}
