bf15d48c1447d5363a3bae2aa845d914838c3a14
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2023-11-03 14:01:35 +0100

    add httpbin - get & post

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod exporters;
 mod datadog_span;
 
+use std::collections::HashMap;
 use actix_web::{get, post, web, App, HttpServer, Responder, HttpRequest, HttpResponse, Error, error};
 use actix_web::web::Bytes;
 use futures::StreamExt;
+use serde_json::json;
 use exporters::{ConsoleExporter, Exporter, FileExporter};
 use datadog_span::DatadogSpan;
 
@@ -33,6 +35,66 @@ async fn traces(body: Bytes, request: HttpRequest) -> Result<HttpResponse, Error
     Ok(HttpResponse::Ok().finish()) // <- send response
 }
 
+#[get("/httpbin/get")]
+async fn httpbin_get(request: HttpRequest) -> Result<HttpResponse, Error> {
+    let query_string = request.query_string();
+    let args: HashMap<String, String> = query_string
+        .split("&")
+        .filter_map(|pair| {
+            if pair.is_empty() {
+                return None;
+            }
+            let mut key_value = pair.split("=");
+            Some((key_value.next().unwrap().to_string(), key_value.next().unwrap().to_string()))
+        })
+        .collect();
+
+    let headers: HashMap<String, String> = request.headers()
+        .iter()
+        .map(|(key, value)| (key.to_string(), value.to_str().unwrap().to_string()))
+        .collect();
+
+    let url = request.uri().to_string();
+
+    Ok(HttpResponse::Ok().json(json!({
+        "args": args,
+        "headers": headers,
+        "url": url,
+    })))
+}
+
+#[post("/httpbin/post")]
+async fn httpbin_post(body: Bytes, request: HttpRequest) -> Result<HttpResponse, Error> {
+    let query_string = request.query_string();
+    let args: HashMap<String, String> = query_string
+        .split("&")
+        .filter_map(|pair| {
+            if pair.is_empty() {
+                return None;
+            }
+            let mut key_value = pair.split("=");
+            Some((key_value.next().unwrap().to_string(), key_value.next().unwrap().to_string()))
+        })
+        .collect();
+
+    let headers: HashMap<String, String> = request.headers()
+        .iter()
+        .map(|(key, value)| (key.to_string(), value.to_str().unwrap().to_string()))
+        .collect();
+
+    let url = request.uri().to_string();
+
+    let string_data = String::from_utf8(body.to_vec()).unwrap();
+
+    Ok(HttpResponse::Ok().json(json!({
+        "args": args,
+        "data": string_data,
+        "headers": headers,
+        "url": url,
+    })))
+}
+
+
 #[get("/")]
 async fn index() -> impl Responder {
     "Hello from dd-localstack!"
@@ -45,6 +107,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(traces)
             .service(index)
+            .service(httpbin_get)
+            .service(httpbin_post)
 
     })
         .bind(("127.0.0.1", 8126))?
